3c368faf43107601ceb19542c3fc943fee1f3019
ehml79 <ehml79@foxmail.com>
2018-05-03 15:18:35 +0800

    Update wechat.py

--- wechat.py
+++ wechat.py
@@ -1,15 +1,31 @@
+# 获取所有微信好友头像
 import itchat
+import os
+import PIL.Image as Image
 
+try:
+    os.mkdir('D:/imgs')
+except:
+    pass
+
+# 打印输出信息
 @itchat.msg_register(itchat.content.TEXT)
 def print_content(msg):
     print(msg['Text'])
 
+# 图片二维码
 itchat.auto_login(hotReload=True)
+
+# 字符串二维码
+# itchat.auto_login(enableCmdQR=True)
+
+
 # itchat.send(u'测试消息发送','filehelper')
 
 friends = itchat.get_friends()
 print(friends)
 
+# 获取所有微信好友头像
 
 friends = itchat.get_friends(update=True)[0:]
 user = friends[0]["UserName"]
@@ -24,5 +40,3 @@ for i in friends:
 
 
 itchat.run()
-
-
